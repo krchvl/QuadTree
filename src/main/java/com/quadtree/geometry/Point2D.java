d03c9ce14e69f72c7f5e921b2231a486358e2255
Vladislav <vladoqqik@icloud.com>
2025-09-17 17:04:49 +0300

    did point class

--- src/main/java/com/quadtree/geometry/Point2D.java
+++ src/main/java/com/quadtree/geometry/Point2D.java
@@ -1,4 +1,26 @@
 package com.quadtree.geometry;
 
 public final class Point2D {
+    private final double x;
+    private final double y;
+
+    public Point2D(double x, double y) {
+        if (Double.isNaN(x) || Double.isNaN(y) ||
+                Double.isInfinite(x) || Double.isInfinite(y)) {
+            throw new IllegalArgumentException("Значения x и y должны быть конечными!");
+        }
+
+        this.x = x;
+        this.y = y;
+    }
+
+    public double x() { return x; }
+    public double y() { return y; }
+
+    public double distanceSquaredTo(double ox, double oy) {
+        double dx = x - ox;
+        double dy = y - oy;
+
+        return dx * dx + dy * dy;
+    }
 }
